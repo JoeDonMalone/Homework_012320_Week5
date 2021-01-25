8bef59d62ed133a2ff27d0fa90eca62d2c1186ba
Joe Malone <joe.d.malone@gmail.com>
2021-01-24 23:32:01 -0600

    All rows have been hard coded via html, compare time to current hour function created, more psuedoCode add

--- Develop/js.js
+++ Develop/js.js
@@ -1,31 +1,60 @@
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+$(document).ready(function() {
+
+    // 
+    // 
+    // 
+    // 
+    // 
+    // 
 
 
-var currentDate = $("#currentDay")
-// var currentDate = document.querySelector("#currentDay")
-currentDate.text(moment().format("dddd, MMMM Do YYYY"))
-// currentDate.textContent = moment().format("dddd, MMMM Do YYYY")
-// console.log(currentDate)
-// = 
-// nineHourViewTime = moment().subtract(9, 'hours').toDate()
-var now = moment().toDate()
-var fourHoursBack = moment().subtract(4, 'hours').toDate()
-var fourHoursForward = moment().add(4, 'hours').toDate()
-console.log(fourHoursBack);
-console.log(now);
-console.log(fourHoursForward);
+    var currentDate = $("#currentDay")
+    // Getting the current hour
+    // moment().format('HH')
+    // var currentDate = document.querySelector("#currentDay")
+    currentDate.text(moment().format("dddd, MMMM Do YYYY"))
+    // currentDate.textContent = moment().format("dddd, MMMM Do YYYY")
+    // console.log(currentDate)
+    // = 
+    // nineHourViewTime = moment().subtract(9, 'hours').toDate()
+    var now = moment().toDate()
+    var fourHoursBack = moment().subtract(4, 'hours').toDate()
+    var fourHoursForward = moment().add(4, 'hours').toDate()
+    console.log(fourHoursBack);
+    console.log(now);
+    console.log(fourHoursForward);
 
-// set Local Storage
-//Create the content layout for the page - Complete
-//Create the script necessary to dynamically add more rows, via jQuery 3 past, 1 current, 3 future
-// assign colors based off of time (if hour-of-day >past: Gray, if(HOD)==present: red else==future: green, )
-// It does not need to be a dynamic calendar whos rows scroll by as the clock ticks. Simply when the page opens, it needs to:
-//  1) Compare the current time, 
-//  2) Determine color of the row based off of time of day
-//  3) set the values of the middle rows, according to their {hourOfDayTask":Description}
+    // set Local Storage
+    //Create the content layout for the page - Complete
+    //Create the script necessary to dynamically add more rows, via jQuery 3 past, 1 current, 3 future
+
+    function hourCheck(hour){
+        var currentHour = moment().format('HH')
+        if (hour == currentHour ){
+            console.log('Current hour: red');
+        } else if(hour>currentHour) {
+            console.log('Future hour: green');
+        } else if(hour<currentHour) {
+            console.log('Past hours: gray');
+        }
+        //compare each hour of day to see which rows are <, =, < current hour
+        // (All hours less than current hour=Gray)
+        // (hour equal to current hour = red)
+        // (All hours greater than current hour=Green)
+    }
+
+    var hourArr= [];
+    for(var i = 0;i<24;i++){
+        var currentHour = moment().format('HH')
+        console.log(currentHour);
+        hourCheck(i)
+        hourArr.push(i)
+    }
+    // hourArr.forEach(hourCheck())
+    // console.log(hourArr)
+    // assign colors based off of time (if hour-of-day >past: Gray, if(HOD)==present: red else==future: green, )
+    // It does not need to be a dynamic calendar whos rows scroll by as the clock ticks. Simply when the page opens, it needs to:
+    //  1) Compare the current time, 
+    //  2) Determine color of the row based off of time of day
+    //  3) set the values of the middle rows, according to their {hourOfDayTask":Description}
+});
