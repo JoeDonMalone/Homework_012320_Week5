b754fa3ce41ed5e3cd4e92538a5cac6a53e0a8a8
Joe Malone <joe.d.malone@gmail.com>
2021-01-25 22:03:39 -0600

    after some variations of testing, have decided to hard code times

--- Develop/js.js
+++ Develop/js.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
 
     // 
-    // 
-    // 
-    // 
-    // 
-    // 
-
-
     var currentDate = $("#currentDay")
     // Getting the current hour
     // moment().format('HH')
@@ -30,23 +23,20 @@ $(document).ready(function() {
 
     function hourCheck(hour){
         var currentHour = moment().format('HH');
-        console.log('Index Hour: ', hour);
-        // $( this ).addClass('present')
+        // console.log('Index Hour: ', hour);
         // if (hour == currentHour ){
-
-
+        //     $( this ).addClass('present')
+        // }
         //     console.log('Current hour: red');
         // } else if(hour>currentHour) {
         //     console.log('Future hour: green');
         // } else if(hour<currentHour) {
         //     console.log('Past hours: gray');
         // }
-    $( ".time-block" ).each(function( index ) {
-        $( this ).text(currentHour)
-        console.log('div row index: ', index)
+
+        // console.log('div row index: ', index)
         // hourCheck(index)
         // console.log( index + ": " + $( this ).text() );
-      });
         // //compare each hour of day to see which rows are <, =, < current hour
         // (All hours less than current hour=Gray)
         // (hour equal to current hour = red)
@@ -55,11 +45,24 @@ $(document).ready(function() {
 
     for(var i = 0;i<24;i++){
         var currentHour = moment().format('HH')
-        hourCheck(i)
+        // console.log(currentHour)
+        // hourCheck(i)
     }
 
+    $( ".time-hr" ).each(function( index ) {
+        $( this ).addClass('data-row-id-'+ index);
+        if($(this).attr('class') == 'col-1 time-hr data-row-id-3') {
+            // $( this ).text(currentHour+"PM");
+            $( this ).addClass('present')
+        };
+      });
 
-
+    $( ".task" ).each(function( index ) {
+        $( this ).text("Task " + index);
+        $( this ).addClass('data-row-id-'+ index);
+        if($(this).attr('class') == 'col-10 task data-row-id-3') {
+            $( this ).addClass('present')}
+        })
 
     // hourArr.forEach(hourCheck())
     // console.log(hourArr)
@@ -68,4 +71,18 @@ $(document).ready(function() {
     //  1) Compare the current time, 
     //  2) Determine color of the row based off of time of day
     //  3) set the values of the middle rows, according to their {hourOfDayTask":Description}
+
+//     function timeSplit() { 
+//         var countDown = 4;
+//         var countup = 4;
+//         for (var i=0; i<4;i++){
+//             countDown -= 1;
+//             console.log(countDown);
+//             countup += 1
+//             console.log(countup);
+//         }
+//     }
+// timeSplit();
+
+
 });
