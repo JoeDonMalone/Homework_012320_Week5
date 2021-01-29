b7ac68a7d8e06c2468cf66d873d2f6a012b326bf
Joe Malone <joe.d.malone@gmail.com>
2021-01-28 21:37:38 -0600

    File touch ups

--- Develop/js.js
+++ Develop/js.js
@@ -1,31 +1,20 @@
 $(document).ready(function() {
 
-    // 
+
     var currentDate = $("#currentDay")
-    // Getting the current hour
-    // moment().format('HH')
+
     currentDate.text(moment().format("dddd, MMMM Do YYYY"));
-    var currentHour = moment().format('HH');
-    
-    // nineHourViewTime = moment().subtract(9, 'hours').toDate()
-    var now = moment().toDate()
-    console.log(now)
-    // var fourHoursBack = moment().subtract(4, 'hours').toDate()
-    // var fourHoursForward = moment().add(4, 'hours').toDate()
-//  value to compare remove string from time object
-    var am_pm = moment().format('a');
     var currentHour = moment().format('HH');
 
     // set Local Storage
-    //Create the content layout for the page 
-    //Create the script necessary to dynamically add more rows, via jQuery 3 past, 1 current, 3 future
+    //Create the content layout for the page
 
     var taskList = JSON.parse(localStorage.getItem('taskList'));        
     if (!taskList) {
         var taskList = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
         localStorage.setItem('taskList', JSON.stringify(taskList));
     } 
-
+    // Add class and id information for page elements using the index for future expansion
     $( ".hour" ).each(function( index ) {
         $(this).attr("id","hour-"+(index+9));
         $( this ).addClass('data-row-id-'+ index)
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Personal Web Daily Scheduler
 # Viewed Here:
 https://joedonmalone.github.io/Homework_012321_Week5/
+
 git hub Repo: https://github.com/JoeDonMalone/Homework_012321_Week5
 # User Notes:
 This is meant to be an online, user-friendly, tool to schedule a 9 hour work/business day.
